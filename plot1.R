7d71ee98beb4eb9e165317fe778a4c6404bd3367
funhawk012 <62207584+funhawk012@users.noreply.github.com>
2020-03-22 21:01:38 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,14 +1,11 @@
+if (!"load_data.R" %in% list.files()) {
+setwd("~/exdata_data_household_power_consumption.txt/")
+} 
 plot1 <- function(data=NULL) {
-    if(is.null(data))
-        data <- load_data()
-    
-    png("plot1.png", width=480, height=480)
-    
-    hist(data$Global_active_power,
-         main="Global Active Power",
-         xlab="Global Active Power (kilowatts)",
-         ylab="Frequency",
-         col="red")
-    
-    dev.off()
+if(is.null(data))
+data <- load_data()
+png(file="plot1.png", width = 480,height = 480)
+plot(1:10)
+hist(data$Global_active_power,main="Global Active Power", xlab="Global Active Power (kilowatts)",ylab="Frequency",col="red")
+dev.off()
 }
